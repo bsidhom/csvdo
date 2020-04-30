445da5838dac51d4b3b9260d3f2de1398eb7e0dc
Ben Sidhom <bsidhom@gmail.com>
2020-04-29 23:38:55 -0700

    Simple tool to execute commands from a CSV
    This is meant to act approximately like xargs, but treating each field
    in a CSV record as a command line argument instead of using whitespace
    or NULs to determine boundaries.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bsidhom/csvdo
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	os.Exit(realMain())
+}
+
+func realMain() int {
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "usage: %s <command>\n", os.Args[0])
+		return 2
+	}
+	command := os.Args[1]
+	reader := csv.NewReader(os.Stdin)
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			return 0
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error reading input: %v\n", err)
+			return 1
+		}
+		cmd := exec.Command(command, record...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err = cmd.Run()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "could not execute %q: %v", record, err)
+			return 1
+		}
+	}
+}
